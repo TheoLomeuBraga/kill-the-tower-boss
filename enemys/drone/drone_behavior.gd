c791b4addcdc606469421a07b759394afb6cf2d1
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2026-07-16 08:13:37 -0300

    add drone enemy

--- enemys/drone/drone_behavior.gd
+++ enemys/drone/drone_behavior.gd
@@ -12,6 +12,12 @@ class_name DroneBehavior
 
 static var rng : RandomNumberGenerator = RandomNumberGenerator.new()
 
+@export var desired_distances : Vector3
+
+@onready var muzles : Array[Node3D] = [$"../muzle_r",$"../muzle_l"]
+
+@export var gun_info : GunInfo
+
 var is_player_visible : bool = false
 func check_player_visibility() -> bool:
 	is_player_visible = false
@@ -23,7 +29,15 @@ func check_player_visibility() -> bool:
 	
 	return is_player_visible
 
+var atack_animation_progresion : float :
+	set(value):
+		atack_animation_progresion = value
+		$"../AnimationTree".set("parameters/Blend2/blend_amount",value)
+
 var state : Callable = idle_state
+
+func get_player_distance() -> float:
+	return body.global_position.distance_to(Player.player.global_position)
 
 func die_state() -> void:
 	state = func(delta:float): return
@@ -37,11 +51,69 @@ func idle_state(delta:float) -> void:
 func folow_state(delta:float) -> void:
 	navegator.target_position = Player.player.global_position + (Vector3.UP * 2.0)
 	navegator.is_navegating = true
+	
+	if get_player_distance() < desired_distances.y:
+		state = atack_state
+	
+	atack_animation_progresion = move_toward(atack_animation_progresion,0.0,delta+2.0)
+
+var current_muzle : int = 0
+var cool_down : float = 0.0
+
+func shot() -> void:
+	
+	current_muzle = (current_muzle+1) % 2
+	var muzle : Node3D = muzles[current_muzle]
+	
+	if not gun_info:
+		return
+	
+	cool_down = gun_info.fire_rate
+	
+	muzle.look_at(Player.player.global_position)
+	muzle.rotation.y = PI
+	
+	var spwn_effect : Node3D = gun_info.projectile_info.spawn_effect.instantiate()
+	muzle.add_child(spwn_effect)
+	spwn_effect.transform = muzle.transform
+	
+	for i : int in gun_info.bullets_per_shot:
+		
+		var projectile : ProjectBehavior = ProjectBehavior.new()
+		add_child(projectile)
+		projectile.global_position = muzle.global_position
+		projectile.muzle_position = muzle.global_position
+		
+		projectile.target_position = muzle.global_basis.z * -100.0
+		
+		projectile.global_rotation = muzle.global_rotation
+		
+		var spread : float = gun_info.spread
+		var vec_spread : Vector3 = Vector3(rng.randf_range(-1.0,1.0),rng.randf_range(-1.0,1.0),rng.randf_range(-1.0,1.0))
+		if vec_spread.length() > 1.0:
+			vec_spread = vec_spread.normalized()
+		vec_spread /= 1.0
+		var aditional_rot : Vector3 = vec_spread * spread
+		projectile.rotate_x(aditional_rot.x)
+		projectile.rotate_y(aditional_rot.y)
+		projectile.rotate_z(aditional_rot.z)
+		
+		projectile.data = gun_info.projectile_info
+		projectile.start()
+	
+
+
 
 func atack_state(delta:float) -> void:
 	navegator.is_navegating = false
 	
+	atack_animation_progresion = move_toward(atack_animation_progresion,1.0,delta+2.0)
 	
+	if get_player_distance() > desired_distances.z:
+		state = folow_state
+	
+	if cool_down < 0.0:
+		shot()
 
 func dash_state(delta:float) -> void:
 	navegator.is_navegating = false
@@ -61,6 +133,9 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if Player.player:
+		
+		cool_down -= delta
+		
 		state.call(delta)
 		
 		rotation_reference.look_at(Player.player.global_position)
