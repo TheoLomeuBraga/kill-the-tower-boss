dd88f4229497d84db001d250e6fe5662ee053546
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2026-07-27 12:32:11 -0300

    put wepons on dedicated view port

---
+++ player/gun_viewport_manager.gd
@@ -0,0 +1,23 @@
+extends SubViewportContainer
+
+@export_flags_3d_render var traget_wepon_flag: int = 1 << 1
+
+@export var player_model : PlayerModel
+
+func update_res() -> void:
+	$SubViewport.size = get_viewport().get_visible_rect().size
+	
+
+func _ready() -> void:
+	return
+	get_viewport().size_changed.connect(update_res)
+	update_res()
+
+func set_render_flag(target:Node) -> void:
+	for n : Node in target.get_children():
+		if n is GeometryInstance3D:
+			var gi : GeometryInstance3D = n
+			gi.layers = traget_wepon_flag
+
+func _process(delta: float) -> void:
+	set_render_flag($"../../Camera3D/PlayerModel")
