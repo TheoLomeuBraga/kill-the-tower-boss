25f757d23042204e1c438307735cc032c5442656
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2026-08-22 13:25:30 -0300

    fix some visual problems

--- menus/settings/option/option.gd
+++ menus/settings/option/option.gd
@@ -16,71 +16,71 @@ func change_float(value:float,name:String) -> void:
 	Settings.change_setting(name,value)
 
 func disable_visibilitys() -> void:
-	$HBoxContainer/Button.visible = false
-	$HBoxContainer/Label.visible = false
-	$HBoxContainer/slider_info.visible = false
-	$HBoxContainer/Control.visible = false
-	$HBoxContainer/Control/CheckBox.visible = false
-	$HBoxContainer/Control/OptionButton.visible = false
-	$HBoxContainer/Control/HSlider.visible = false
+	$Button.visible = false
+	$Label.visible = false
+	$slider_info.visible = false
+	$Control.visible = false
+	$Control/CheckBox.visible = false
+	$Control/OptionButton.visible = false
+	$Control/HSlider.visible = false
 
 func setup_as_button(name:String,callable:Callable) -> void:
 	disable_visibilitys()
-	$HBoxContainer/Button.visible = true
-	$HBoxContainer/Button.text = name
-	$HBoxContainer/Button.pressed.connect(callable)
+	$Button.visible = true
+	$Button.text = name
+	$Button.pressed.connect(callable)
 
 func setup_as_check_box(name:String,setting_name:String,base_value:bool) -> void:
 	disable_visibilitys()
-	$HBoxContainer/Label.visible = true
-	$HBoxContainer/Label.text = name
+	$Label.visible = true
+	$Label.text = name
 	
-	$HBoxContainer/Control.visible = true
-	$HBoxContainer/Control/CheckBox.visible = true
-	$HBoxContainer/Control/CheckBox/CheckBox.button_pressed = base_value
-	$HBoxContainer/Control/CheckBox/CheckBox.toggled.connect(change_bool.bind(setting_name))
+	$Control.visible = true
+	$Control/CheckBox.visible = true
+	$Control/CheckBox/CheckBox.button_pressed = base_value
+	$Control/CheckBox/CheckBox.toggled.connect(change_bool.bind(setting_name))
 
 func setup_as_option(name:String,setting_name:String,base_value:int,options:Array[String]) -> void:
 	disable_visibilitys()
-	$HBoxContainer/Label.visible = true
-	$HBoxContainer/Label.text = name
+	$Label.visible = true
+	$Label.text = name
 	
-	$HBoxContainer/Control.visible = true
-	$HBoxContainer/Control/OptionButton.visible = true
+	$Control.visible = true
+	$Control/OptionButton.visible = true
 	
 	for s:String in options:
-		$HBoxContainer/Control/OptionButton.add_item(s)
+		$Control/OptionButton.add_item(s)
 	
-	$HBoxContainer/Control/OptionButton.selected = base_value
+	$Control/OptionButton.selected = base_value
 	
-	$HBoxContainer/Control/OptionButton.item_selected.connect(change_int.bind(setting_name))
+	$Control/OptionButton.item_selected.connect(change_int.bind(setting_name))
 
 var minimun_slider_chars : int = 0
 func setup_as_slider(name:String,setting_name:String,base_value:float,min:float,max:float,step:float,minimun_chars:int) -> void:
 	disable_visibilitys()
-	$HBoxContainer/Label.visible = true
-	$HBoxContainer/Label.text = name
+	$Label.visible = true
+	$Label.text = name
 	
-	$HBoxContainer/slider_info.visible = true
-	$HBoxContainer/slider_info.text = str(base_value)
+	$slider_info.visible = true
+	$slider_info.text = str(base_value)
 	
-	$HBoxContainer/Control.visible = true
-	$HBoxContainer/Control/HSlider.visible = true
-	$HBoxContainer/Control/HSlider.min_value = min
-	$HBoxContainer/Control/HSlider.max_value = max
-	$HBoxContainer/Control/HSlider.step = step
-	$HBoxContainer/Control/HSlider.value = base_value
+	$Control.visible = true
+	$Control/HSlider.visible = true
+	$Control/HSlider.min_value = min
+	$Control/HSlider.max_value = max
+	$Control/HSlider.step = step
+	$Control/HSlider.value = base_value
 	
 	minimun_slider_chars = minimun_chars
 	
-	$HBoxContainer/Control/HSlider.value_changed.connect(change_float.bind(setting_name))
+	$Control/HSlider.value_changed.connect(change_float.bind(setting_name))
 
 func _process(delta: float) -> void:
-	if $HBoxContainer/slider_info.visible:
-		$HBoxContainer/slider_info.text = str($HBoxContainer/Control/HSlider.value)
+	if $slider_info.visible:
+		$slider_info.text = str($Control/HSlider.value)
 		
-		while $HBoxContainer/slider_info.text.length() < minimun_slider_chars:
-			$HBoxContainer/slider_info.text = "0"+$HBoxContainer/slider_info.text
+		while $slider_info.text.length() < minimun_slider_chars:
+			$slider_info.text = "0"+$slider_info.text
 		
-		if $HBoxContainer/Control/HSlider.step == floor($HBoxContainer/Control/HSlider.step):
-			$HBoxContainer/slider_info.text = $HBoxContainer/slider_info.text.split(".")[0]
+		if $Control/HSlider.step == floor($Control/HSlider.step):
+			$slider_info.text = $slider_info.text.split(".")[0]
--- menus/settings/settings_menu.gd
+++ menus/settings/settings_menu.gd
@@ -3,7 +3,7 @@ class_name SettingsMenu
 
 @export var title : String
 
-@onready var options_container : Node = $VBoxContainer/Control/PanelContainer/VBoxContainer
+@onready var options_container : Node = $VBoxContainer/Control/PanelContainer/ScrollContainer/VBoxContainer
 
 func create_option() -> SettingOption:
 	var ret : SettingOption = load("res://menus/settings/option/option.tscn").instantiate()
@@ -15,6 +15,27 @@ func _ready() -> void:
 	$VBoxContainer/Label.text = title
 	
 	var option : SettingOption
+	
+	option = create_option()
+	option.setup_as_button("a",print.bind("a"))
+	
+	option = create_option()
+	option.setup_as_check_box("box","a",false)
+	
+	option = create_option()
+	option.setup_as_check_box("box","b",true)
+	
+	option = create_option()
+	option.setup_as_option("option","c",0,["a","b","c"])
+	
+	option = create_option()
+	option.setup_as_option("option","d",1,["a","b","c"])
+	
+	option = create_option()
+	option.setup_as_slider("slide","e",50,0,100,0.1,4)
+	
+	option = create_option()
+	option.setup_as_slider("slide","f",100,0,100,1,3)
 	
 	option = create_option()
 	option.setup_as_button("a",print.bind("a"))
