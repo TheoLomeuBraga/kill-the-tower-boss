1644ce690c5b012adf4ba00c676cca3cbdef7711
Théo <63592879+TheoLomeuBraga@users.noreply.github.com>
2026-09-18 09:07:06 -0300

    make enemys capable of open doors

--- scripts/trigers/area_triger.gd
+++ scripts/trigers/area_triger.gd
@@ -10,20 +10,59 @@ var used : bool = false
 
 var sync_data : Dictionary
 
-func on_body_entered(body:Node3D) -> void:
-	
-	if not body is Player:
-		return
-	
-	if one_use and used:
-		return
-	
+@export var detect_stats_entities : bool = false
+
+func get_stats_from_node(node:Node3D) -> Stats:
+	for c : Node in node.get_children():
+		if c is Stats:
+			return c
+	return null
+
+func use() -> void:
 	for np : NodePath in targets:
 		get_node(np).triger()
 	
 	if one_use:
 		used = true
 		sync_data["used"] = true
+
+@export var bodys_in : Array[Node3D]
+
+func on_body_entered(body:Node3D) -> void:
+	
+	if detect_stats_entities:
+		if not get_stats_from_node(body):
+			return
+	else:
+		if not body is Player:
+			return
+	
+	if one_use and used:
+		return
+	
+	if bodys_in.size() == 0:
+		use()
+	
+	bodys_in.push_back(body)
+	
+	
+
+func on_body_exit(body:Node3D) -> void:
+	
+	if detect_stats_entities:
+		if not get_stats_from_node(body):
+			return
+	else:
+		if not body is Player:
+			return
+	
+	if one_use and used:
+		return
+	
+	if bodys_in.size() == 1:
+		use()
+	
+	bodys_in.erase(body)
 
 func _ready() -> void:
 	
@@ -39,4 +78,4 @@ func _ready() -> void:
 	if triger_on_enter:
 		body_entered.connect(on_body_entered)
 	if triger_on_exit:
-		body_exited.connect(on_body_entered)
+		body_exited.connect(on_body_exit)
